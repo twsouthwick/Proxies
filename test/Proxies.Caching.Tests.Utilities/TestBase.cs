85c5a0b5ae956c7f132c06011f37e921d3214496
Taylor Southwick <twsouthwick@outlook.com>
2018-11-09 06:05:54 -0800

    Add test base

---
+++ test/Proxies.Caching.Tests.Utilities/TestBase.cs
@@ -0,0 +1,51 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoFixture;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Proxies.Caching.Tests
+{
+    public abstract class TestBase : IClassFixture<TestScopeProvider>, IDisposable
+    {
+        private readonly ILifetimeScope _scope;
+
+        protected virtual Fixture Fixture { get; }
+
+        public TestBase(TestScopeProvider scopeProvider)
+        {
+            _scope = scopeProvider.Container.BeginLifetimeScope(AddServices);
+            Fixture = new Fixture();
+
+            Customize(Fixture);
+        }
+
+        private void AddServices(ContainerBuilder builder)
+        {
+            var services = new ServiceCollection();
+
+            CustomizeServices(services);
+
+            builder.Populate(services);
+        }
+
+        protected virtual void Customize(Fixture fixture)
+        {
+        }
+
+        protected virtual void CustomizeServices(IServiceCollection services)
+        {
+        }
+
+        protected T Resolved<T>()
+        {
+            return _scope.Resolve<T>();
+        }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
+        }
+    }
+}
---
+++ test/Proxies.Caching.Tests.Utilities/TestScopeProvider.cs
@@ -0,0 +1,50 @@
+﻿using Autofac;
+using Autofac.Builder;
+using Autofac.Core;
+using Autofac.Features.ResolveAnything;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Proxies.Caching.Tests
+{
+    public sealed class TestScopeProvider : IDisposable
+    {
+        public TestScopeProvider()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+            builder.RegisterSource(new NSubstituteRegistrationSource());
+
+            Container = builder.Build();
+        }
+
+        public void Dispose()
+        {
+            Container.Dispose();
+        }
+
+        internal IContainer Container { get; }
+
+        private class NSubstituteRegistrationSource : IRegistrationSource
+        {
+            public bool IsAdapterForIndividualComponents => false;
+
+            public IEnumerable<IComponentRegistration> RegistrationsFor(Autofac.Core.Service service, Func<Autofac.Core.Service, IEnumerable<IComponentRegistration>> registrationAccessor)
+            {
+                if (service is IServiceWithType ts && (ts.ServiceType.IsInterface || ts.ServiceType.IsAbstract))
+                {
+                    var builder = RegistrationBuilder.ForDelegate((c, p) => Substitute.For(new[] { ts.ServiceType }, null))
+                        .As(service)
+                        .InstancePerLifetimeScope();
+
+                    return new[] { builder.CreateRegistration() };
+                }
+
+                return Enumerable.Empty<IComponentRegistration>();
+            }
+        }
+    }
+}
